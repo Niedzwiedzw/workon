3fae01ce0b914e9c2932557aee3ad21a6b45d4b1
niedzwiedzw <niedzwiedzwo@gmail.com>
2021-05-25 10:43:49 +0200

    execute programs first

--- src/terminal.rs
+++ src/terminal.rs
@@ -25,31 +25,6 @@ impl RunTerminal for Alacritty {
 
 pub fn startup<T: RunTerminal>(project: &ProjectConfig) -> WorkonResult<()> {
     let mut tasks = vec![];
-    for terminal_command in project.terminals.iter() {
-        println!(":: starting up :: {}", project.project_name);
-        let mut base_command = Command::new(T::exec_name());
-        let mut args = vec![
-            T::working_dir_arg().to_string(),
-            terminal_command.workdir.to_string_lossy().to_string(),
-            T::run_command_arg().to_string(),
-        ];
-        args.append(&mut terminal_command.command.clone());
-
-        tasks.push(std::thread::spawn(move || {
-            let command = base_command.args(&args[..]);
-            eprintln!(
-                ":: executing >> {} {}",
-                command.get_program().to_string_lossy(),
-                command
-                    .get_args()
-                    .map(|c| c.to_string_lossy())
-                    .collect::<Vec<_>>()
-                    .join(" ")
-            );
-            command.status().unwrap();
-        }));
-    }
-
     for program_command in project.programs.iter() {
         println!(":: starting up :: {}", project.project_name);
         let mut base_command = Command::new(T::exec_name());
@@ -75,11 +50,34 @@ pub fn startup<T: RunTerminal>(project: &ProjectConfig) -> WorkonResult<()> {
         }));
     }
 
-    tasks.into_iter().for_each(|t| {
-        match t.join() {
-            Ok(_) => println!(":: DONE :: "),
-            Err(e) => eprintln!("an error occured {:?}", e),
-        }
+    for terminal_command in project.terminals.iter() {
+        println!(":: starting up :: {}", project.project_name);
+        let mut base_command = Command::new(T::exec_name());
+        let mut args = vec![
+            T::working_dir_arg().to_string(),
+            terminal_command.workdir.to_string_lossy().to_string(),
+            T::run_command_arg().to_string(),
+        ];
+        args.append(&mut terminal_command.command.clone());
+
+        tasks.push(std::thread::spawn(move || {
+            let command = base_command.args(&args[..]);
+            eprintln!(
+                ":: executing >> {} {}",
+                command.get_program().to_string_lossy(),
+                command
+                    .get_args()
+                    .map(|c| c.to_string_lossy())
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            );
+            command.status().unwrap();
+        }));
+    }
+
+    tasks.into_iter().for_each(|t| match t.join() {
+        Ok(_) => println!(":: DONE :: "),
+        Err(e) => eprintln!("an error occured {:?}", e),
     });
 
     Ok(())
